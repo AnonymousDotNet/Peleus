4c966768e2ed453e168fb9b3b1b004f5319ce241
AnonymousDotNet <18776095145@163.com>
2018-11-09 18:50:56 +0800

    update

--- Peleus/Program.cs
+++ Peleus/Program.cs
@@ -6,6 +6,7 @@ using System.Data.SqlClient;
 using SqlUtil;
 using System;
 using Html;
+using Peleus.Script;
 
 namespace Peleus
 {
@@ -23,43 +24,54 @@ namespace Peleus
             //}
 
 
-            //统计局
-            string url = string.Format("http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/index.html");
-
-
-            var source = Http.Downloader.Download(url);
-            if (!string.IsNullOrEmpty(source))
-            {
-                HtmlDocument doc = new HtmlDocument();
-                doc.LoadHtml(source);
-
-                //var name_Provinces = XpathUtil.GetTexts(doc.DocumentNode, "//tr[@class='provincetr']/td/a", true, "|");
-                var name_Provinces = XpathUtil.GetHtmls(doc.DocumentNode, "//tr[@class='provincetr']/td/a");
-
-                var name_Provinces_Url = XpathUtil.GetAttributes(doc.DocumentNode, "//tr[@class='provincetr']/td/a", "href");
-
-                
-                foreach (var item in name_Provinces)
-                {
-                    if (!string.IsNullOrEmpty(item))
-                    {
-
-                    }
-                }
-            }
 
 
 
+            ////统计局
+            //string url = string.Format("http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/index.html");
+
+
+            //var source = Http.Downloader.Download(url);
+            //if (!string.IsNullOrEmpty(source))
+            //{
+            //    HtmlDocument doc = new HtmlDocument();
+            //    doc.LoadHtml(source);
+
+            //    //var name_Provinces = XpathUtil.GetTexts(doc.DocumentNode, "//tr[@class='provincetr']/td/a", true, "|");
+            //    var name_Provinces = XpathUtil.GetHtmls(doc.DocumentNode, "//tr[@class='provincetr']/td/a");
+
+            //    var name_Provinces_Url = XpathUtil.GetAttributes(doc.DocumentNode, "//tr[@class='provincetr']/td/a", "href");
+
+
+            //    foreach (var item in name_Provinces)
+            //    {
+            //        if (!string.IsNullOrEmpty(item))
+            //        {
+
+            //        }
+            //    }
+            //}
 
 
 
 
-            string sql = "";
 
-            string con = ConfigurationManager.AppSettings["Database"];//获取配置文件里的数据库地址信息，这里默认获取当前应用的配置文件，不能新建一个配置文件就能使用，当然好像也可以用新增的配置文件只是没有了解过，就暂时先这么写了。
 
-            //SqlConnection database = MsSqlUtil.Connect(Config.DB);//这个跟上面的是一样的，只是用了封装好的DLL。
-            var id = SqlClient.GetData(sql);
+
+            //string sql = "";
+
+            //string con = ConfigurationManager.AppSettings["Database"];//获取配置文件里的数据库地址信息，这里默认获取当前应用的配置文件，不能新建一个配置文件就能使用，当然好像也可以用新增的配置文件只是没有了解过，就暂时先这么写了。
+
+            ////SqlConnection database = MsSqlUtil.Connect(Config.DB);//这个跟上面的是一样的，只是用了封装好的DLL。
+            //var id = SqlClient.GetData(sql);
+
+
+
+
+            //国家信息
+            Countries_Zone countries = new Countries_Zone();
+            countries.Download();//实例化时类的访问权限可写可不写，但是类中的方法不能使用静态类型，否则无法引用。
+            Console.ReadKey();
         }
 
     }
---
+++ Peleus/Script/Countries_Zone.cs
@@ -0,0 +1,49 @@
+﻿using Html;
+using Peleus.Utility;
+using System;
+using System.Collections.Generic;
+
+namespace Peleus.Script
+{
+    class Countries_Zone
+    {
+        public void Download()
+        {
+            string url = string.Format("http://gx.189.cn/mall/detail/html/gjdmqh.html");
+            string source = Http.Downloader.Download(url);
+            if (!string.IsNullOrEmpty(source))
+            {
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(source);
+
+                string Xpath_Countries = "";
+                string CN = "";
+                string EN = "";
+                string Abbr = "";
+                string CallCode = "";
+                string TimeZone;//这里需要转换成decimal(2, 1)类型
+                DateTime CreateTime;
+
+                for (int i = 3; i < 195; i++)
+                {
+                    Xpath_Countries = string.Format("//body/table[1]/tr[{0}]/td", i);
+                    
+                    EN = XpathUtil.GetText(doc.DocumentNode, string.Format(Xpath_Countries + "[1]"));
+                    CN = XpathUtil.GetText(doc.DocumentNode, string.Format(Xpath_Countries + "[2]"));
+                    Abbr = XpathUtil.GetText(doc.DocumentNode, string.Format(Xpath_Countries + "[3]"));
+                    CallCode = XpathUtil.GetText(doc.DocumentNode, string.Format(Xpath_Countries + "[4]"));
+                    TimeZone = XpathUtil.GetText(doc.DocumentNode, string.Format(Xpath_Countries + "[5]"));
+
+                    CreateTime = DateTime.Now;
+
+
+                }
+                //var name_Provinces_Url = XpathUtil.GetAttributes(doc.DocumentNode, "//tr[@class='provincetr']/td/a", "href");
+            }
+        }
+        public void Sql_Counties()
+        {
+
+        }
+    }
+}
