be56df765eb35d5fa2e8988a86aebe1c0d2605a6
AnonymousDotNet <18776095145@163.com>
2018-11-10 15:07:00 +0800

    update

--- Peleus/Script/Countries_Zone.cs
+++ Peleus/Script/Countries_Zone.cs
@@ -21,7 +21,7 @@ namespace Peleus.Script
                 string EN = "";
                 string Abbr = "";
                 string CallCode = "";
-                string TimeZone;//这里需要转换成decimal(2, 1)类型
+                int TimeZone;//这里需要转换成decimal(2, 1)类型
                 DateTime CreateTime;
 
                 for (int i = 3; i < 195; i++)
@@ -32,10 +32,15 @@ namespace Peleus.Script
                     CN = XpathUtil.GetText(doc.DocumentNode, string.Format(Xpath_Countries + "[2]"));
                     Abbr = XpathUtil.GetText(doc.DocumentNode, string.Format(Xpath_Countries + "[3]"));
                     CallCode = XpathUtil.GetText(doc.DocumentNode, string.Format(Xpath_Countries + "[4]"));
-                    TimeZone = XpathUtil.GetText(doc.DocumentNode, string.Format(Xpath_Countries + "[5]"));
+                    TimeZone = int.Parse(XpathUtil.GetText(doc.DocumentNode, string.Format(Xpath_Countries + "[5]")));
 
                     CreateTime = DateTime.Now;
-
+                    Cache.Countries_Zone countries_Zone = new Cache.Countries_Zone();
+                    countries_Zone.CN = CN;
+                    countries_Zone.EN = EN;
+                    countries_Zone.Abbr = Abbr;
+                    countries_Zone.Callcode = CallCode;
+                    countries_Zone.TimeZone = TimeZone;
 
                 }
                 //var name_Provinces_Url = XpathUtil.GetAttributes(doc.DocumentNode, "//tr[@class='provincetr']/td/a", "href");
